cc3af9ff33d6c81dbd584865a1761570e64301ed
Joost van der Waal <joost.vanderwaal@aegon.nl>
2024-05-27 17:36:04 +0200

    linting test cases

--- workspaces/aws-custom-resources/src/index._test_.ts
+++ workspaces/aws-custom-resources/src/index._test_.ts
@@ -1,34 +1,32 @@
 import https from 'https';
-jest.mock('https');
 
+import { cfRequest, functionContext } from './_tests_/mocks';
+import { wrapHandler } from '.';
+
+jest.mock('https');
 const requestMock = https.request as jest.Mock;
 const writeMock = jest.fn();
-
 requestMock.mockImplementation((_req: any, cb: any) => {
   setTimeout(() => cb({
     on: cb,
     statusCode: 200,
-    statusMessage: 'API Success'
+    statusMessage: 'API Success',
   }), 10);
 
-  return {on: () => { }, end: () => { }, write: writeMock,
-  }
-})
-
-import { wrapHandler } from '.';
-import { cfRequest, functionContext } from './_tests_/mocks';
+  return { on: () => { }, end: () => { }, write: writeMock,
+  };
+});
 
 describe('Core functionality', () => {
   it('Should normally complete a request', async () => {
-
     const handler = wrapHandler(async () => {
       return {};
     });
-    
+
     await handler(cfRequest('Update'), functionContext(), () => {});
 
-    expect(requestMock).toHaveBeenCalledWith(expect.objectContaining({ method: "PUT", hostname: "responseurl.com"}), expect.any(Function));
-    expect(writeMock).toHaveBeenNthCalledWith(1, expect.stringContaining('{"Status":"SUCCESS"'))
+    expect(requestMock).toHaveBeenCalledWith(expect.objectContaining({ method: 'PUT', hostname: 'responseurl.com' }), expect.any(Function));
+    expect(writeMock).toHaveBeenNthCalledWith(1, expect.stringContaining('{"Status":"SUCCESS"'));
   });
 
   it('Should catch an error', async () => {
@@ -39,55 +37,55 @@ describe('Core functionality', () => {
 
     await handler(cfRequest('Update'), functionContext(), () => { });
 
-    expect(writeMock).toHaveBeenNthCalledWith(2, expect.stringContaining('{"Status":"FAILED","Reason":"My Error'))
+    expect(writeMock).toHaveBeenNthCalledWith(2, expect.stringContaining('{"Status":"FAILED","Reason":"My Error'));
   });
 
   it('Should detect a timeout and send before killed', async () => {
     console.error = jest.fn();
     const handler = wrapHandler(async () => {
       await new Promise(resolve => setTimeout(resolve, 2000));
-      return {}
+      return {};
     });
 
     await handler(cfRequest('Update'), functionContext(), () => { });
 
-    expect(writeMock).toHaveBeenNthCalledWith(3, expect.stringContaining('{"Status":"FAILED","Reason":"Custom Resource timeout'))
+    expect(writeMock).toHaveBeenNthCalledWith(3, expect.stringContaining('{"Status":"FAILED","Reason":"Custom Resource timeout'));
   });
 
   it('Should return a failed create physical ID', async () => {
     console.error = jest.fn();
     const handler = wrapHandler(async () => {
-      throw new Error('Create failed')
+      throw new Error('Create failed');
     });
 
     await handler(cfRequest('Create'), functionContext(), () => { });
 
-    expect(writeMock).toHaveBeenNthCalledWith(5, expect.stringContaining('{"Status":"FAILED","Reason":"Create failed'))
-    expect(writeMock).toHaveBeenNthCalledWith(5, expect.stringContaining('"PhysicalResourceId":"aws-custom-resources:CREATE_FAILED_PHYSICAL_ID"'))
-  });  
+    expect(writeMock).toHaveBeenNthCalledWith(5, expect.stringContaining('{"Status":"FAILED","Reason":"Create failed'));
+    expect(writeMock).toHaveBeenNthCalledWith(5, expect.stringContaining('"PhysicalResourceId":"aws-custom-resources:CREATE_FAILED_PHYSICAL_ID"'));
+  });
 
   it('Should ignore delete errors when the create failed', async () => {
     console.error = jest.fn();
     const handler = wrapHandler(async () => {
-      throw new Error('Create failed')
+      throw new Error('Create failed');
     });
 
     await handler(cfRequest('Delete', {
-      PhysicalResourceId: 'aws-custom-resources:CREATE_FAILED_PHYSICAL_ID'
+      PhysicalResourceId: 'aws-custom-resources:CREATE_FAILED_PHYSICAL_ID',
     }), functionContext(), () => { });
 
-    expect(writeMock).toHaveBeenNthCalledWith(6, expect.stringContaining('{"Status":"SUCCESS"'))
+    expect(writeMock).toHaveBeenNthCalledWith(6, expect.stringContaining('{"Status":"SUCCESS"'));
   });
 
   it('Should return physical id based on log stream on create when not provided', async () => {
     console.error = jest.fn();
     const handler = wrapHandler(async () => {
-      return {}
+      return {};
     });
 
     await handler(cfRequest('Create'), functionContext(), () => { });
 
-    expect(writeMock).toHaveBeenNthCalledWith(7, expect.stringContaining('{"Status":"SUCCESS"'))
-    expect(writeMock).toHaveBeenNthCalledWith(7, expect.stringContaining('"PhysicalResourceId":"logStreamName"'))
-  });  
+    expect(writeMock).toHaveBeenNthCalledWith(7, expect.stringContaining('{"Status":"SUCCESS"'));
+    expect(writeMock).toHaveBeenNthCalledWith(7, expect.stringContaining('"PhysicalResourceId":"logStreamName"'));
+  });
 });
